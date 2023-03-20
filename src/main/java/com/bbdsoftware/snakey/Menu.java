8ef8a9a02260153555156b6dad0094c941416425
Palesa Mpe <palesa@bbd.co.za>
2023-03-20 21:00:58 +0200

    code cleanup

--- src/main/java/com/bbdsoftware/snakey/Menu.java
+++ src/main/java/com/bbdsoftware/snakey/Menu.java
@@ -3,36 +3,43 @@ package com.bbdsoftware.snakey;
 import javax.swing.*;
 
 public class Menu extends Board {
-    JButton startButton = new JButton("Start Game");
-    JButton quitButton = new JButton("Quit");
-    JButton leaderboardButton = new JButton("Leaderboard");
+    JButton startButton;
+    JButton quitButton;
+    JButton leaderboardButton;
 
-    Menu(){
-        startButton.setBounds(250,50,200,40);
-        leaderboardButton.setBounds(250,100,200,40);
-        quitButton.setBounds(250,150,200,40);
+    Menu() {
+        startButton = new JButton("Start Game");
+        quitButton = new JButton("Quit");
+        leaderboardButton = new JButton("Leaderboard");
     }
 
-    void startMenu(){
+    private void startMenu() {
+        startButton.setBounds(250, 50, 200, 40);
+        leaderboardButton.setBounds(250, 100, 200, 40);
+        quitButton.setBounds(250, 150, 200, 40);
+
         board.add(startButton);
         board.add(leaderboardButton);
         board.add(quitButton);
+    }
 
-      startButton.addActionListener(e -> {
-          System.out.println("clicked");
-          //get game board
-      });
-      leaderboardButton.addActionListener(e -> {
-          System.out.println("clicked");
-          //get game board
-      });
-      quitButton.addActionListener(e -> {
-          int input = JOptionPane.showConfirmDialog(null,
-                  "Are you sure you want to quit?","Select option",JOptionPane.YES_NO_OPTION);
-          if (input==0){
-              System.exit(0);
-          }
-      });
+    void processMenu() {
+        startMenu();
+        startButton.addActionListener(e -> {
+            System.out.println("clicked");
+            //get game board
+        });
+        leaderboardButton.addActionListener(e -> {
+            System.out.println("clicked");
+            //get game board
+        });
+        quitButton.addActionListener(e -> {
+            int input = JOptionPane.showConfirmDialog(null,
+                    "Are you sure you want to quit?", "Select option", JOptionPane.YES_NO_OPTION);
+            if (input == 0) {
+                System.exit(0);
+            }
+        });
     }
 
 }
