b6948367498781b6066eba5da244f980fc1e1008
Palesa Mpe <palesa@bbd.co.za>
2023-03-21 20:19:50 +0200

    Adding game panel with keyListener from MovementController

--- src/main/java/com/bbdsoftware/snakey/GamePanel.java
+++ src/main/java/com/bbdsoftware/snakey/GamePanel.java
@@ -1,52 +1,51 @@
 package com.bbdsoftware.snakey;
 
+import com.bbdsoftware.snakey.controllers.MovementController;
 import com.bbdsoftware.snakey.domain.Cell;
 import com.bbdsoftware.snakey.domain.Food;
 import com.bbdsoftware.snakey.domain.Orange;
-import com.bbdsoftware.snakey.enums.Direction;
+import com.bbdsoftware.snakey.domain.Snake;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     static final int unit = 25;
-    static final int gameUnit = (600 * 600) / unit;
     Food orange;
+    static final int screenWidth=600;
+    static final int screenHeight = 600;
 
-    final int movex[] = new int[gameUnit];
-    final int movey[] = new int[gameUnit];
-    int screenWidth,screenHeight=600;
-    Direction direction = Direction.RIGHT;
+    final int gameUnit = (screenHeight * screenWidth) / unit;
+    final int coordinateX[] = new int[gameUnit];
+    final int coordinateY[] = new int[gameUnit];
+
     int speed = 100;
     boolean moving = false;
     Timer timer = new Timer(speed, this);
 
     Snake player = new Snake();
 
-
     ArrayList<Rectangle> snakeBlocks = new ArrayList<>();
 
     GamePanel() {
         this.setBackground(Color.BLACK);
-        this.setPreferredSize(new Dimension(600, 600));
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setFocusable(true);
-        this.addKeyListener(new Keyboard());
+        this.addKeyListener(new MovementController(player));
         timer.start();
         startGame();
     }
 
-    public void startGame() {
-snakeBlocks.add(new Rectangle(10,10));
-        generateFood();
+    private void startGame() {
+        snakeBlocks.add(new Rectangle(10, 10));
         moving = true;
-
+        generateFood();
     }
 
     public void paintComponent(Graphics g) {
@@ -57,147 +56,74 @@ snakeBlocks.add(new Rectangle(10,10));
     public void draw(Graphics g) {
         g.setColor(Color.red);
         g.fillOval(orange.getFoodCell().getX(), orange.getFoodCell().getY(), unit, unit);
-        System.out.println(snakeBlocks.size());
-
 
         for (int i = 0; i < snakeBlocks.size(); i++) {
             if (i == 0) {
                 g.setColor(Color.green);
-                g.fillRect(movex[i], movey[i],unit,unit);
+                g.fillRect(coordinateX[i], coordinateY[i], unit, unit);
             } else {
                 g.setColor(new Color(45, 180, 0));
-                g.fillRect(movex[i], movey[i], unit, unit);
+                g.fillRect(coordinateX[i], coordinateY[i], unit, unit);
             }
-            checkFood(movex[i], movey[i]);
-            //checkWall(movex[i],movey[i]);
+            checkFood(coordinateX[i], coordinateY[i]);
         }
-
-        if (!player.isAlive()){
-            g.fillRect(300, 300, unit, unit);
-            g.setColor(Color.gray);
-            moving=false;
-
-            JLabel l = new JLabel("You lose");
-            l.setBounds(100,100,100,100);
-
-            add(l);
-  }
-//        for (int i = 0; i <= snakeBlocks.size(); i++) {
-//            if (i == 0) {
-//                g.setColor(Color.green);
-//                g.fillRect(movex[i], movey[i], unit, unit);
-//            } else {
-//                g.setColor(new Color(45, 180, 0));
-//                g.fillRect(movex[i], movey[i], unit, unit);
-//
-//            }
-//
-//            checkFood(movex[i],movey[i]);
-//        }
 
     }
 
     public void generateFood() {
         Random random = new Random();
-        int x = random.nextInt(575) + 1;
-
-        while (x % 25 != 0) {
-            x = random.nextInt(575) + 1;
-        }
-        int y = random.nextInt(575) + 1;
-        while (y % 25 != 0) {
-            y = random.nextInt(575) + 1;
-        }
+        int x = Math.round((random.nextInt(screenWidth-unit))/25)*25;
+        int y = Math.round((random.nextInt(screenHeight-unit))/25)*25;
         orange = new Orange(new Cell(x, y));
     }
 
 
-
     public void checkFood(int x, int y) {
         if (x == orange.getFoodCell().getX() && y == orange.getFoodCell().getY()) {
-           snakeBlocks.add(new Rectangle(25,25));
+            snakeBlocks.add(new Rectangle(unit, unit));
             player.eatFood(orange);
             generateFood();
         }
     }
 
-    public void gameOver(){
-        JLabel label = new JLabel("You Lose!");
-
+    public void gameOver() {
+        if (!player.isAlive()) {
+            moving = false;
+            JLabel label = new JLabel("You lose");
+            label.setBounds(100, 100, 100, 100);
+            add(label);
+        }
     }
+
     public void move() {
         for (int i = snakeBlocks.size(); i > 0; i--) {
-            movex[i] = movex[i - 1];
-            movey[i] = movey[i - 1];
+            coordinateX[i] = coordinateX[i - 1];
+            coordinateY[i] = coordinateY[i - 1];
         }
 
-        switch (direction) {
+        switch (player.getSnakeDirection()) {
             case UP:
-                movey[0] = movey[0] - unit;
+                coordinateY[0] = coordinateY[0] - unit;
                 break;
             case DOWN:
-                movey[0] = movey[0] + unit;
+                coordinateY[0] = coordinateY[0] + unit;
                 break;
             case LEFT:
-                movex[0] = movex[0] - unit;
+                coordinateX[0] = coordinateX[0] - unit;
                 break;
             case RIGHT:
-                movex[0] = movex[0] + unit;
+                coordinateX[0] = coordinateX[0] + unit;
                 break;
         }
     }
 
-    //        public void moveSnake(Direction direction){
-//        int new_y = player.x[0];
-//        int new_x =player.y[0];
-//
-//        if (direction.equals(Direction.UP)){
-//            new_y -= 1;
-//        }
-//        else if (direction.equals(Direction.DOWN)){
-//            new_y += 1;
-//        }
-//        else if (direction.equals(Direction.LEFT)){
-//            new_x -= 1;
-//        }
-//        else{
-//            new_x += 1;
-//        }
-//
-//    }
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (moving){
+        if (moving) {
             move();
             repaint();
         }
 
-
     }
-
-
-    public class Keyboard extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-            super.keyPressed(e);
-
-            //Movements here
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_DOWN:
-                    direction = Direction.DOWN;
-                    break;
-                case KeyEvent.VK_UP:
-                    direction = Direction.UP;
-                    break;
-                case KeyEvent.VK_LEFT:
-                    direction = Direction.LEFT;
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    direction = Direction.RIGHT;
-                    break;
-            }
-        }
-    }
-
 
 }
