ed90da589cb5401b7e06120f3aced07aa3363de9
Raaga Krishna <raaga@bbd.co.za>
2023-03-21 17:30:45 +0200

    Changed Food way of implementation of Food class

--- src/main/java/com/bbdsoftware/snakey/domain/Apple.java
+++ src/main/java/com/bbdsoftware/snakey/domain/Apple.java
@@ -3,54 +3,12 @@ package com.bbdsoftware.snakey.domain;
 import com.bbdsoftware.snakey.enums.Colour;
 
 public class Apple extends Food{
-    private final String foodName;
-    private final float score;
-    private final Colour foodColour;
-    private final int length;
-    private final Cell foodCell;
-
-    public Apple(Cell food_cell){
-        this.foodName = "Apple";
-        this.score = 0.5F;
-        this.foodColour = Colour.RED;
-        this.length = 2;
-
-        this.foodCell = food_cell;
+    public Apple(Cell foodCell){
+        super("Apple", 0.5F, Colour.RED, 2, foodCell);
     }
 
     @Override
-    public String getFoodName() {
-        return foodName;
-    }
+    public void doSpecial(Snake snake) {
 
-    @Override
-    public float getScore() {
-        return score;
-    }
-
-    @Override
-    public Colour getFoodColour() {
-        return foodColour;
-    }
-
-    @Override
-    public int getLength() {
-        return length;
-    }
-
-    @Override
-    public Cell getFoodCell() {
-        return foodCell;
-    }
-
-    @Override
-    public String toString() {
-        return "Apple{" +
-                "foodName='" + foodName + '\'' +
-                ", score=" + score +
-                ", foodColour=" + foodColour +
-                ", length=" + length +
-                ", foodCell=" + foodCell +
-                '}';
     }
 }
--- src/main/java/com/bbdsoftware/snakey/domain/Orange.java
+++ src/main/java/com/bbdsoftware/snakey/domain/Orange.java
@@ -3,54 +3,12 @@ package com.bbdsoftware.snakey.domain;
 import com.bbdsoftware.snakey.enums.Colour;
 
 public class Orange  extends Food{
-    private final String foodName;
-    private final float score;
-    private final Colour foodColour;
-    private final int length;
-    private final Cell foodCell;
-
     public Orange(Cell foodCell){
-        this.foodName = "Orange";
-        this.score = 0.9F;
-        this.foodColour = Colour.ORANGE;
-        this.length = 5;
-
-        this.foodCell = foodCell;
+        super("Orange", 0.9F, Colour.ORANGE, 5, foodCell);
     }
 
     @Override
-    public String getFoodName() {
-        return foodName;
-    }
+    public void doSpecial(Snake snake) {
 
-    @Override
-    public float getScore() {
-        return score;
-    }
-
-    @Override
-    public Colour getFoodColour() {
-        return foodColour;
-    }
-
-    @Override
-    public int getLength() {
-        return length;
-    }
-
-    @Override
-    public Cell getFoodCell() {
-        return foodCell;
-    }
-
-    @Override
-    public String toString() {
-        return "Orange{" +
-                "foodName='" + foodName + '\'' +
-                ", score=" + score +
-                ", foodColour=" + foodColour +
-                ", length=" + length +
-                ", foodCell=" + foodCell +
-                '}';
     }
 }
