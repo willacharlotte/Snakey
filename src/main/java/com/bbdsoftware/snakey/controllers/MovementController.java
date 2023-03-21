ad4aa8e1597e4ee8cc8f8652e2b29ba9a3dea098
Willa Lyle <willa@bbd.co.za>
2023-03-21 15:14:22 +0200

    feat: update keyPressed override to switch over keyCode and to include input from arrow keys

--- src/main/java/com/bbdsoftware/snakey/controllers/MovementController.java
+++ src/main/java/com/bbdsoftware/snakey/controllers/MovementController.java
@@ -1,47 +1,42 @@
 package com.bbdsoftware.snakey.controllers;
 
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;;
+import java.awt.event.KeyListener;
+
+import com.bbdsoftware.snakey.domain.Snake;;
 
 public class MovementController implements KeyListener {
-  public static void main(String[] args) {
-   
-  }
+    private final Snake snake;
 
-  public MovementController() { // add snake instance here?
-
-  }
-
-  @Override
-  public void keyTyped(KeyEvent e) {
-    // not needed
-  }
-
-  @Override
-  public void keyPressed(KeyEvent e) {
-    switch (e.getKeyChar()) {
-      case 'w':
-        System.out.println("I moved up!");
-        // snake.moveUp() ??
-        break;
-      case 'a':
-        System.out.println("I moved left!");
-        // snake.moveLeft() ??
-        break;
-      case 's':
-        System.out.println("I moved down!");
-        // snake.moveDown() ??
-        break;
-      case 'd':
-        System.out.println("I moved right!");
-        // snake.moveRight() ??
-        break;
+    public MovementController(Snake snake) {
+        this.snake = snake;
     }
-  }
 
-  @Override
-  public void keyReleased(KeyEvent e) {
-    // not needed (yet?)
-  }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println(e.getKeyCode());
+        switch (e.getKeyCode()) {
+            case 87: case 38:
+                System.out.println("I moved up!");
+                break;
+            case 65: case 37:
+                System.out.println("I moved left!");
+                break;
+            case 83: case 40:
+                System.out.println("I moved down!");
+                break;
+            case 68: case 39:
+                System.out.println("I moved right!");
+                break;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
 
 }
