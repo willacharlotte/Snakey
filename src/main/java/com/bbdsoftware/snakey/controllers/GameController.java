3357d57b7401315aa341b240a2d9a26e7738579a
Palesa Mpe <palesa@bbd.co.za>
2023-03-24 09:02:24 +0200

    game controller

--- src/main/java/com/bbdsoftware/snakey/controllers/GameController.java
+++ src/main/java/com/bbdsoftware/snakey/controllers/GameController.java
@@ -3,7 +3,6 @@ package com.bbdsoftware.snakey.controllers;
 import com.bbdsoftware.snakey.domain.Cell;
 import com.bbdsoftware.snakey.domain.Food;
 import com.bbdsoftware.snakey.domain.Snake;
-import com.bbdsoftware.snakey.enums.FoodTypes;
 import com.bbdsoftware.snakey.domain.User;
 import com.bbdsoftware.snakey.presentation.GameFrame;
 import com.bbdsoftware.snakey.presentation.LeaderboardFrame;
@@ -11,8 +10,6 @@ import com.bbdsoftware.snakey.presentation.MenuFrame;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,13 +22,12 @@ import java.util.Random;
 
 public class GameController extends JPanel implements ActionListener {
 
-    static final int numBlocks = 13;
+    static final int numBlocks = 15;
     static final int blockUnit = 50;
     static final int snakeUnit = 50;
-    static final int headingOffsetY = 100;
     static final int screenWidth = numBlocks*blockUnit;
-    static final int screenHeight = numBlocks*blockUnit + headingOffsetY;
-    
+    static final int screenHeight = numBlocks*blockUnit;
+
     Random random = new Random();
     BoardController board = new BoardController(numBlocks);
     Snake snake;
@@ -39,14 +35,13 @@ public class GameController extends JPanel implements ActionListener {
     int foodX, foodY;
 
     int speed = 350;
-    boolean moving = false;
+
     Timer timer = new Timer(speed, this);
     BufferedImage snakeSegmentHorizontal, snakeSegmentVertical;
     BufferedImage snakeHeadUp, snakeHeadDown, snakeHeadLeft, snakeHeadRight;
     BufferedImage snakeTailUp, snakeTailDown, snakeTailLeft, snakeTailRight;
     BufferedImage apple, banana, pear, orange;
     Font pixelFontLarge, pixelFontSmall;
-    JLabel[] foodLabels = new JLabel[5];
 
     public GameController() {
         snake = board.getMySnake();
@@ -61,12 +56,10 @@ public class GameController extends JPanel implements ActionListener {
 
         loadResources();
         timer.start();
-        startGame();
+
     }
 
-    private void startGame() {
-        moving = true;
-    }
+
 
     private void loadResources(){
         try {
@@ -102,12 +95,6 @@ public class GameController extends JPanel implements ActionListener {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        foodLabels[0] = new JLabel("SCORE: 0");
-        foodLabels[1] = new JLabel(" x 0");
-        foodLabels[2] = new JLabel(" x 0");
-        foodLabels[3] = new JLabel(" x 0");
-        foodLabels[4] = new JLabel(" x 0");
     }
     
     public void paintComponent(Graphics g) {
@@ -116,50 +103,47 @@ public class GameController extends JPanel implements ActionListener {
     }
 
     private void draw(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, screenWidth, headingOffsetY);
-
         int snakeCount = 0;
         for(Cell s : snake.getSnakeBlocks()){
             if(snakeCount == snake.getCurrentLength()-1){
                 switch(s.getCellDirection()){
                     case DOWN:
-                        g.drawImage(snakeHeadDown, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeHeadDown, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case UP:
-                        g.drawImage(snakeHeadUp, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeHeadUp, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case LEFT:
-                        g.drawImage(snakeHeadLeft, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeHeadLeft, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case RIGHT:
-                        g.drawImage(snakeHeadRight, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeHeadRight, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                 }
             }else if(snakeCount == 0){
                 switch(s.getCellDirection()){
                     case DOWN:
-                        g.drawImage(snakeTailDown, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeTailDown, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case UP:
-                        g.drawImage(snakeTailUp, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeTailUp, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case LEFT:
-                        g.drawImage(snakeTailLeft, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeTailLeft, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case RIGHT:
-                        g.drawImage(snakeTailRight, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeTailRight, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                 }
             }else{
                 switch(s.getCellDirection()){
                     case DOWN:
                     case UP:
-                        g.drawImage(snakeSegmentVertical, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeSegmentVertical, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                     case LEFT:
                     case RIGHT:
-                        g.drawImage(snakeSegmentHorizontal, s.getX()*blockUnit, s.getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);         
+                        g.drawImage(snakeSegmentHorizontal, s.getX()*blockUnit, s.getY()*blockUnit, snakeUnit, snakeUnit, null);         
                         break;
                 }            }
             snakeCount++;
@@ -169,70 +153,33 @@ public class GameController extends JPanel implements ActionListener {
         if(foodItem != null){
             switch(foodItem.getFoodType()){
                 case APPLE:
-                    g.drawImage(apple, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);
+                    g.drawImage(apple, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit, snakeUnit, snakeUnit, null);
                     break;
                 case BANANA:
-                    g.drawImage(banana, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);
+                    g.drawImage(banana, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit, snakeUnit, snakeUnit, null);
                     break;
                 case PEAR:
-                    g.drawImage(pear, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);
+                    g.drawImage(pear, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit, snakeUnit, snakeUnit, null);
                     break;
                 case ORANGE:
-                    g.drawImage(orange, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit+headingOffsetY, snakeUnit, snakeUnit, null);
+                    g.drawImage(orange, foodItem.getFoodCell().getX()*blockUnit, foodItem.getFoodCell().getY()*blockUnit, snakeUnit, snakeUnit, null);
                     break;
             }
-        }
-
-        foodLabels[0].setText("SCORE: " + snake.getScore());
-        foodLabels[0].setBounds(screenWidth-150, 25, 150, 50);
-        foodLabels[0].setFont(pixelFontSmall);
-        foodLabels[0].setHorizontalAlignment(SwingConstants.LEFT);
-        foodLabels[0].setVerticalAlignment(SwingConstants.CENTER);
-        foodLabels[0].setOpaque(false);
-        foodLabels[0].setForeground(Color.GREEN);
-        add(foodLabels[0]);
-
-        int foodPos = 1;
-        for(FoodTypes f : FoodTypes.values()){
-            switch(f){
-                case APPLE:
-                    foodPos = 1;
-                    g.drawImage(apple, foodPos*105-75, 25, blockUnit, blockUnit, null);
-                    break;
-                case BANANA:
-                    foodPos = 2;
-                    g.drawImage(banana, foodPos*105-75, 25, blockUnit, blockUnit, null);
-                    break;
-                case PEAR:
-                    foodPos = 3;
-                    g.drawImage(pear, foodPos*105-75, 25, blockUnit, blockUnit, null);
-                    break;
-                case ORANGE:
-                    foodPos = 4;
-                    g.drawImage(orange, foodPos*105-75, 25, blockUnit, blockUnit, null);
-                    break;
-            }
-            foodLabels[foodPos].setText(" x " + snake.getFoodItemCount(f));
-            foodLabels[foodPos].setBounds(foodPos*105-35, 25, 50, 50);
-            foodLabels[foodPos].setFont(pixelFontSmall);
-            foodLabels[foodPos].setHorizontalAlignment(SwingConstants.CENTER);
-            foodLabels[foodPos].setVerticalAlignment(SwingConstants.CENTER);
-            foodLabels[foodPos].setOpaque(false);
-            foodLabels[foodPos].setForeground(Color.GREEN);
-            add(foodLabels[foodPos]);
         }
     }
 
     private void gameOver() {
-        moving = false;
+
+        // TO DO: ADD HIGH SCORE HERE
+
         JButton quitButton = new JButton("QUIT");
         JLabel gameOverLabel = new JLabel("Game Over");
         JLabel nameLabel = new JLabel("ENTER YOUR NAME:");
         JTextField nameField = new JTextField(10);
         JButton submitNameButton = new JButton("SUBMIT");
         JButton restartButton = new JButton("RESTART");
-        JLabel scoreLabel = new JLabel("SCORE: " + snake.getScore());
-        addEnterUsernameFields(scoreLabel, quitButton, gameOverLabel, nameLabel, nameField, submitNameButton);
+        addEnterUsernameFields(quitButton, gameOverLabel, nameLabel, nameField, submitNameButton);
+
         submitNameButton.setEnabled(false);
 
         nameField.addKeyListener(new KeyAdapter() {
@@ -261,9 +208,9 @@ public class GameController extends JPanel implements ActionListener {
 
             User user= new User(userName,userScore);
             DatabaseController db = new DatabaseController();
-            db.addScore(user);
+            db.AddPlayerRecord(user);
             new LeaderboardFrame(user);
-            
+
         });
 
         restartButton.addActionListener(e -> {
@@ -271,16 +218,16 @@ public class GameController extends JPanel implements ActionListener {
             closeParentFrame();
         });
 
- 
-        
+
+
         quitButton.addActionListener(e -> {
             new MenuFrame();
             closeParentFrame();
         });
     }
 
-    private void addEnterUsernameFields(JLabel scoreLabel, JButton quitButton, JLabel gameOverLabel, JLabel nameLabel, JTextField nameField, JButton submitNameButton){
-        gameOverLabel.setBounds(0, screenHeight/2-200, screenWidth, 100);
+    private void addEnterUsernameFields(JButton quitButton, JLabel gameOverLabel, JLabel nameLabel, JTextField nameField, JButton submitNameButton){
+        gameOverLabel.setBounds(0, screenHeight/2-300, screenWidth, 100);
         gameOverLabel.setFont(pixelFontLarge);
         gameOverLabel.setHorizontalAlignment(SwingConstants.CENTER);
         gameOverLabel.setVerticalAlignment(SwingConstants.CENTER);
@@ -289,35 +236,26 @@ public class GameController extends JPanel implements ActionListener {
         gameOverLabel.setForeground(Color.GREEN);   
         add(gameOverLabel);
 
-        scoreLabel.setBounds(screenWidth/2-200, screenHeight/2-75, 400, 50);
-        scoreLabel.setFont(pixelFontLarge);
-        scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        scoreLabel.setVerticalAlignment(SwingConstants.CENTER);
-        scoreLabel.setOpaque(true);
-        scoreLabel.setBackground(Color.GRAY);
-        scoreLabel.setForeground(Color.BLACK);  
-        add(scoreLabel);
-
-        nameLabel.setBounds(0, screenHeight/2, 210, 50);
+        nameLabel.setBounds(115, screenHeight/2-175, 200, 50);
         nameLabel.setFont(pixelFontSmall);
-        nameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        nameLabel.setHorizontalAlignment(SwingConstants.LEFT);
         nameLabel.setVerticalAlignment(SwingConstants.CENTER);
         nameLabel.setOpaque(true);
         nameLabel.setBackground(Color.GRAY);
         nameLabel.setForeground(Color.BLACK);  
         add(nameLabel);
 
-        nameField.setBounds(235, screenHeight/2, 200, 50);
+        nameField.setBounds(335, screenHeight/2-175, 300, 50);
         nameField.setFont(pixelFontSmall);
         nameField.setHorizontalAlignment(SwingConstants.LEFT);
         nameField.setOpaque(true);
         nameField.setBackground(Color.WHITE);
-        nameField.setForeground(Color.BLACK); 
-        nameField.setMargin(new Insets(0, 15, 0, 0));
+        nameField.setForeground(Color.BLACK);
+        nameField.setMargin(new Insets(0, 20, 0, 0));
         add(nameField);
 
-        submitNameButton.setBounds(460, screenHeight/2, 150, 50);
-        submitNameButton.setFont(pixelFontSmall);
+        submitNameButton.setBounds(screenWidth/2-150, screenHeight/2-100, 300, 80);
+        submitNameButton.setFont(pixelFontLarge);
         submitNameButton.setHorizontalAlignment(SwingConstants.CENTER);
         submitNameButton.setVerticalAlignment(SwingConstants.CENTER);
         submitNameButton.setOpaque(true);
@@ -325,7 +263,7 @@ public class GameController extends JPanel implements ActionListener {
         submitNameButton.setForeground(Color.GREEN); 
         add(submitNameButton);
 
-        quitButton.setBounds(screenWidth/2-150, screenHeight-250, 300, 80);
+        quitButton.setBounds(screenWidth/2-150, screenHeight/2, 300, 80);
         quitButton.setFont(pixelFontLarge);
         quitButton.setHorizontalAlignment(SwingConstants.CENTER);
         quitButton.setVerticalAlignment(SwingConstants.CENTER);
@@ -334,6 +272,7 @@ public class GameController extends JPanel implements ActionListener {
         quitButton.setForeground(Color.GREEN); 
         add(quitButton);
     }
+
 
     private void addEndOfGameFields(JButton restartButton, JButton quitButton){
         restartButton.setBounds(screenWidth/2-200, screenHeight/2-175, 400, 100);
@@ -380,13 +319,22 @@ public class GameController extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (moving) {
-            if(snake.isAlive()){
-                board.processSnakeMovement(snake.getSnakeDirection());
-            }else{
-                gameOver();
+        if (snake.isAlive()) {
+            board.processSnakeMovement(snake.getSnakeDirection());
+
+            if (snake.getLength() < 15) {
+                timer.setDelay(speed);
+            } else if (snake.getLength() < 25) {
+                timer.setDelay(speed - 100);
+            } else if (snake.getLength() < 50) {
+                timer.setDelay(speed - 150);
+            } else {
+                timer.setDelay(speed - 200);
             }
-            repaint();
+
+        } else {
+            gameOver();
         }
+        repaint();
     }
 }
